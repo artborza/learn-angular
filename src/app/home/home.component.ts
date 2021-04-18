11066c50032f26bbd75f4521d8a88e61db8e98e7
Thummanoon hwancaheam <42062759+artborza@users.noreply.github.com>
2021-04-18 11:06:53 +0700

    update

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,21 @@
+import { sharedStylesheetJitUrl } from '@angular/compiler';
+import {Component, Input} from '@angular/core';
+
+@Component({
+    selector:'app-home',
+    templateUrl:'./home.component.html',
+    styleUrls:['./home.component.css']
+})
+export class HomeComponent{
+    @Input()
+    title:String;
+
+    count =0;
+    items =[1,2,3,4,5,6,7]
+    constructor() {
+
+    }
+    countNumber(){
+        this.count = this.count +1;
+    }
+}
